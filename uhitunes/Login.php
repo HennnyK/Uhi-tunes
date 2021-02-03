4d9971f537abbe0189ee7124f8904f1d89947237
Henny <70945096+HennnyK@users.noreply.github.com>
2021-02-03 14:02:30 +0000

    Add files via upload

---
+++ uhitunes/Login.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>Login.php</title>
+</head>
+<body>
+<h2> Miracle Security System</h2>
+
+<h2>Please enter your Email address and password</h2>
+
+<form method="post" action="Checkuser.php">
+<table>
+<tr>
+<td>Email address :</td>
+<td><input type="text" name="Email" size="50"/></td>
+</tr>
+<tr>
+<td>Password</td>
+<td><input type="password" name="Password" size="15"/></td>
+</tr>
+
+<tr>
+<td colspan="2"><input type="submit" value="Check User"/></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
